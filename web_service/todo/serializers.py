5c27bedccbdc90fe3aa16b05d24199983bdbff52
Tabolich-Aliaksei <100754713+Tabolich-Aliaksei@users.noreply.github.com>
2023-03-16 10:33:10 +0100

    Update serializers.py

--- web_service/todo/serializers.py
+++ web_service/todo/serializers.py
@@ -1,1 +1,24 @@
+from rest_framework.serializers import ModelSerializer, HyperlinkedIdentityField, HyperLinkedRelateField
+from .models import Project
+from .models import Todo
 
+
+class ProjectSerializer(ModelSerializer):
+  # Настройка сериализатора
+  # Настройка Foreign key
+  # owner = HyperlinkedIdentityField(view_name='user-detail')
+  # Настройка Many to many
+  # users = HyperlinkedRelateField(many=True, view_name='user-detail', read_only=True)
+  
+  class Meta:
+    model = Project
+    field = '__all__'
+    
+    
+class TodoSerializer(ModelSerializer):
+  # project = HyperlinkedIdentityField(view_name='project-detail')
+  # creator = HyperlinkedIdentityField(view_name='user-detail')
+  
+  class Mets:
+    model = Todo
+    exclude = ('is_active',)
