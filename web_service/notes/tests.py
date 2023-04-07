4c167bab64d19db48c5e1e6af8609a8b1c93667e
Tabolich-Aliaksei <100754713+Tabolich-Aliaksei@users.noreply.github.com>
2023-04-07 09:40:15 +0200

    Update tests.py

--- web_service/notes/tests.py
+++ web_service/notes/tests.py
@@ -1,4 +1,11 @@
 from django.test import TestCase
+from rest_framework import status
+from rest_frameork.test import APIRequestFactory,force_authenticate,APIclient,APISimpleTestCase,APITestCase
+from mixer.backend-django import mixer
+from django.contrib.auth.midels import User
+
+from notes.views import NoteModelViewSet
+from notes.models import Note
 
 # Create your tests here.
 
@@ -9,3 +16,8 @@ class TestNoteViewSet(TestCase):
 
   def tearDown(self) -> None:
     pass
+  
+  def test_get_list(self):
+    pass
+  
+  
